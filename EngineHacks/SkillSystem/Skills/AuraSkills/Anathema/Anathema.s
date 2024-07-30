e28cb561a3e9b4a8c2afc9158cabaefbc9681518
AlessioCominetti <alessio.cominetti@tiscali.it>
2024-07-30 23:11:01 +0200

    Update Anathema.s

--- EngineHacks/SkillSystem/Skills/AuraSkills/Anathema/Anathema.s
+++ EngineHacks/SkillSystem/Skills/AuraSkills/Anathema/Anathema.s
@@ -20,15 +20,30 @@ cmp r0, #0
 beq Done
 
 mov r0, r4
-add     r0,#0x62    @Move to the attacker's avoid.
+add     r0,#0x60    @Move to the attacker's hit.
+ldrh    r3,[r0]     @Load the attacker's hit into r3.
+sub     r3,#10      @subtract 10 from the attacker's hit
+strh    r3,[r0]     @Store attacker hit
+
+add r0, #2 @avoid
 ldrh    r3,[r0]     @Load the attacker's avoid into r3.
-sub     r3,#10    @subtract 10 from the attacker's avoid
+sub     r3,#10      @subtract 10 from the attacker's avoid
 strh    r3,[r0]     @Store attacker avoid
 
-add r0, #6 @crit avoid
-ldrh    r3,[r0]     @Load the attacker's avoid into r3.
-sub     r3,#10    @subtract 10 from the attacker's avoid
-strh    r3,[r0]     @Store attacker avoid
+add r0, #4 @crit
+ldrh    r3,[r0]     @Load the attacker's crit into r3.
+sub     r3,#10       @subtract 10 from the attacker's crit
+cmp 	r3,#0
+bgt		StoreCritAsIs
+mov 	r3,#0
+StoreCritAsIs:
+strh    r3,[r0]     @Store attacker crit
+
+add r0, #2 @crit avoid
+ldrh    r3,[r0]     @Load the attacker's crit avoid into r3.
+sub     r3,#10      @subtract 10 from the attacker's crit avoid
+strh    r3,[r0]     @Store attacker crit avoid 
+
 
 Done:
 pop {r4-r7}
