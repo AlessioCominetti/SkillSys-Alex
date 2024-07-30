5b45526a8b77b686e3c2131a04f5d1480f6aacf6
AlessioCominetti <alessio.cominetti@tiscali.it>
2024-07-30 23:12:28 +0200

    Update Focus.s

--- EngineHacks/SkillSystem/Skills/AuraSkills/Focus/Focus.s
+++ EngineHacks/SkillSystem/Skills/AuraSkills/Focus/Focus.s
@@ -19,12 +19,12 @@ ldr r1, FocusID
 cmp r0, #0
 beq Done
 
-@Check if there are allies in 3 spaces
+@Check if there are allies in 5 spaces
 ldr r0, GetUnitsInRange
 mov lr, r0
 mov r0, r4 @attacker
 mov r1, #0 @allies
-mov r2, #3 @range
+mov r2, #5 @range
 .short 0xf800
 cmp r0, #0
 bne Done
@@ -32,7 +32,7 @@ bne Done
 mov r0, r4
 add     r0,#0x66    @Move to the attacker's crit.
 ldrh    r3,[r0]     @Load the attacker's crit into r3.
-add     r3,#10    @add 10.
+add     r3,#100    @add 100.
 strh    r3,[r0]     @Store.
 
 Done:
